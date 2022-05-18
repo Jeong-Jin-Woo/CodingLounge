10dbcbae8adb809c3530e786beda61dd435cc2e8
zjaym1703 <jym971024@gmail.com>
2022-05-18 18:46:20 +0900

    [UPDATE] 1.post.html : 소스코드 에디터 추가 2.profile.html : 프로필 이미지 추가

--- passport/index.js
+++ passport/index.js
@@ -14,11 +14,11 @@ module.exports = () => {
       where: { id },
       include: [{
         model: User,
-        attributes: ['id', 'nick'],
+        attributes: ['id', 'nick', 'user_image'],
         as: 'Followers',
       }, {
         model: User,
-        attributes: ['id', 'nick'],
+        attributes: ['id', 'nick','user_image'],
         as: 'Followings',
       }, 
     ],
